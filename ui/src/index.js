19c48f9d3a9621ce0e6e228324895fc4760f4450
Sander Hautvast <shautvast@gmail.com>
2020-12-04 17:18:33 +0100

    repaired the gui

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Tree from './Tree';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Tree />, document.getElementById('root'));
-registerServiceWorker();
